b96d8fc5809425dfe40f5520849052da2fd1a51b
root <aldo.mor@gmail.com>
2016-07-05 21:13:40 -0500

    Initial commit

---
+++ app/src/main/java/com/aldomora/misnotificiaciones/MainActivity.java
@@ -0,0 +1,32 @@
+package com.aldomora.misnotificiaciones;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void receiveNotification(View v){
+        String token = FirebaseInstanceId.getInstance().getToken();
+        sendTokenReg(token);
+    }
+    public void sendTokenReg(String token){
+        Log.d("TOKEN",token);
+    }
+}
